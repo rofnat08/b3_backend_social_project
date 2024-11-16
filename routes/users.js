ef0900828a39865c1c1277e76fdba037fa8bb8cb
rofnat08 <rofna08@gmail.com>
2024-11-16 18:50:46 -0500

    profile y update

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { testUser, register, login } from "../controllers/user.js";
+import { testUser, register, login, profile, listUsers, updateUser} from "../controllers/user.js";
+import { ensureAuth } from '../middlewares/auth.js';
 
 const router = Router();
 
 // Definir rutas de user
-router.get('/test-user', testUser);
+router.get('/test-user', ensureAuth, testUser);
 router.post('/register', register);
 router.post('/login', login);
+router.get('/profile/:id', ensureAuth, profile);
+router.get('/list/:page?', ensureAuth, listUsers);
+router.put('/update', ensureAuth, updateUser);
 
 //Exportar el Router
 export default router;
